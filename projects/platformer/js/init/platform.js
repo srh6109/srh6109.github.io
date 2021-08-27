713d1bd18a5a71ccd7990ebedc784e3d6240ba81
srh6109 <srh6109@ppsbstudent.org>
2021-08-27 18:46:53 +0000

    game

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -37,8 +37,13 @@
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
 
         // example:
-        createPlatform(400, 250);
-        createPlatform(170, 460, 0.5);
+        createPlatform(0, 500, 0.8);
+        createPlatform(550, 600, 0.8);
+        createPlatform(500, 200);
+        createPlatform(400, 380, 0.4);
+        createPlatform(800, 380, 0.5);
+        createPlatform(0, 270, 0.7);
+        
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
